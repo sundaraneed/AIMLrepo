af7d5086236b800a5871f7e697c141ebffe63185
sravani <srilakshmisravani7@gmail.com>
2023-09-10 20:57:27 +0530

    commit

---
+++ src/first/program.java
@@ -0,0 +1,14 @@
+package first;
+
+public class program {
+	public int add(int num1,int num2)
+	{
+		int r= num1 + num2;
+		return r;
+		}
+public int sub(int num1, int num2)
+{
+	int r= num1 - num2;
+	return r;
+}
+}
